d4f0ac98794dcff1313cb42559f3387b7be72fea
YBinh Nguyen <nybinh201196@gmail.com>
2023-01-04 22:56:37 +0700

    Init

--- src/Lab_02/Lab02_03.java
+++ src/Lab_02/Lab02_03.java
@@ -6,17 +6,17 @@ public class Lab02_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please input your weight (kg): ");
-        double YourWeight = scanner.nextFloat();
+        float yourWeight = scanner.nextFloat();
         System.out.print("Please input your height (m): ");
-        double YourHeight = scanner.nextFloat();
-        double BMI = YourWeight / (YourHeight * 2);
+        float yourHeight = scanner.nextFloat();
+        float BMI = yourWeight / (yourHeight * 2);
 
-        if (BMI <= 18.5) {
+        if (BMI <= 18.5f) {
             System.out.println("You are underweight");
             System.out.println("You should increase your weight");
-        } else if (BMI < 24.9) {
+        } else if (BMI < 24.9f) {
             System.out.println("You are normal weight");
-        } else if (BMI < 29.9) {
+        } else if (BMI < 29.9f) {
             System.out.println("You are Overweight");
         } else {
             System.out.println("You are Obesity");
